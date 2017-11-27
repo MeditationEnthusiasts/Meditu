9f0556b56bb3d85222a33ab3afd41cabc0719f26
Seth Hendrick <seth@shendrick.net>
2017-11-26 22:15:07 -0500

    Hello world of the GUI.

---
+++ MeditationLogger/MeditationLogger.Gui/Startup.cs
@@ -0,0 +1,75 @@
+﻿//
+// Meditation Logger.
+// Copyright (C) 2017  Seth Hendrick.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeditationLogger.Gui
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices( IServiceCollection services )
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure( IApplicationBuilder app, IHostingEnvironment env )
+        {
+            if( env.IsDevelopment() )
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc( 
+                routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}" );
+                } 
+            );
+
+            this.Bootstrap();
+        }
+
+
+        public async void Bootstrap()
+        {
+            var options = new BrowserWindowOptions
+            {
+                WebPreferences = new WebPreferences
+                {
+                    WebSecurity = false
+                }
+            };
+
+            await Electron.WindowManager.CreateWindowAsync( options );
+        }
+    }
+}
