baf64f1c6d458fbab972eba45d3a6a61e5c6fcd9
Seth Hendrick <seth@shendrick.net>
2020-10-18 21:51:05 -0400

    UseMvc options for 3.1

--- MeditationLogger/MeditationLogger.Gui/Startup.cs
+++ MeditationLogger/MeditationLogger.Gui/Startup.cs
@@ -33,7 +33,9 @@ namespace MeditationLogger.Gui
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices( IServiceCollection services )
         {
-            services.AddMvc();
+            services.AddMvc(
+                options => options.EnableEndpointRouting = false
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
